0996b3cb0e2735849f08e370ea2f490a0a0c5bac
Ko-GyeongTae <kokt0203@naver.com>
2022-06-05 00:54:43 +0900

    Feat: ENV 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,15 +7,18 @@ import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      database: 'micro_product',
-      username: 'kevin',
-      password: null,
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
       entities: ['dist/**/*.entity.{ts,js}'],
-      synchronize: true, // never true in production!
+      synchronize: process.env.NODE_ENV == 'dev' ? true : false,
     }),
     ProductModule,
   ],
